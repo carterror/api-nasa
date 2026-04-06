69264fe9d05d120f225fcfd55b3aacd9492cf437
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2026-04-06 08:47:26 -0500

    Correct syntax in sitemap.ts
    Fix missing closing brace and add newline at end of file.

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://love.carana26.com/boda',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://love.carana26.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://love.carana26.com/one',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
